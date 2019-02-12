52d26af523e23b8b09a42ed9d675bdfb4759cd63
Donaim <3Donaim@gmail.com>
2019-02-12 10:36:45 +0100

    recursion works

--- inliner.py
+++ inliner.py
@@ -2,21 +2,30 @@
 import parser
 from parser import *
 
-def anyeqrec(leafs: list, bind: Bind) -> bool:
-	def anyeq(leaf: Leaf):
-		if leaf.unique_id == bind.unique_id:
-			return True
-		else:
-			return anyeqrec(leafs=leaf.leafs, bind=bind)
-	if len(leafs) == 0:
+def anyeqrec(target: Leaf, bind: Bind, mem: dict) -> bool:
+	if target.unique_id in mem:
+		return True
+	else:
+		mem[target.unique_id] = True
+
+	if target is None:
 		return False
-	return any ( map (anyeq, leafs) )
+	if type(target) is Bind:
+		print('visiting {}; its target is {};'.format(target.name, type(target.target)))
+		if target == bind:
+			return True
+		return anyeqrec(target.target, bind, mem) or any ( [ anyeqrec(l, bind=bind, mem=mem) for l in target.leafs ] )
+	else:
+		return any ( [ anyeqrec(l, bind=bind, mem=mem) for l in target.leafs ] )
 
 def is_binding_recursive(bind: Bind) -> bool:
 	if bind.target is None:
 		return False
+	if bind.target == bind:
+		return True
 	else:
-		return anyeqrec(leafs=bind.target.leafs, bind=bind)
+		print('has {} leafs: {}'.format(bind.name, len(bind.leafs) > 0))
+		return anyeqrec(target=bind.target, bind=bind, mem = {})
 
 def get_leaf_text(le: Leaf) -> str:
 	t = type(le)

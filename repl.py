75b9660e0753fcb18b070bb74e5167177f5d16c9
Donaim <3Donaim@gmail.com>
2019-02-23 16:42:57 +0100

    working, huh?

--- repl.py
+++ repl.py
@@ -24,10 +24,10 @@ TFLAGS = [
 	'--no-make-inline',
 	'--do-caching',
 	'--no-print-intermediate',
-	'--count-total-exec',
+	'--no-count-total-exec',
 	'--no-show-debug',
 	'--use-typeid',
-	'--echo-expr',
+	'--no-echo-expr',
 	'--flagsfile', '{PROJ}/flags.h',
 	'--headerfile', '{PROJ}/header.h',
 	'--declare-file', '{PROJ}/declare.h',
@@ -36,21 +36,38 @@ TFLAGS = [
 ]
 TFLAGS = [s.format(PROJ=PROJ) for s in TFLAGS]
 
+def is_binding(line: str) -> bool:
+	(pre, _, p) = line.partition(' ')
+	(p2, mid, post) = p.strip().partition('=')
+	return post
+
 def kcompile():
 	subprocess.check_call(['./lambda-cpp.py'] + TFLAGS)
 	subprocess.check_call([CC, '-o', 'repl.exe', '-O0', DEST, PROJ + '/header.c'])
 	subprocess.check_call(['./repl.exe'])
 
 def loop(file):
+	buffor = []
 	while True:
 		inp = input('> ')
 		inp = (inp.replace('\n', ' ')).strip()
-		if inp == 'exit':
+		if inp == '#exit':
 			break
 
-		file.write(inp + '\n\n')
+		buffor.append(inp + '\n')
+
+		file.seek(0, 0)
+		file.truncate(0)
+		file.writelines(buffor)
 		file.flush()
-		kcompile()
+
+		if not is_binding(inp):
+			try:
+				kcompile()
+			except:
+				print('Repl sees error')
+
+			buffor.pop()
 
 def setup(args, callback):
 	mode = 'w' if args.refresh else 'a'

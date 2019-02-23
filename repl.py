ef5a32e2d666c9755b2143098ed7383cd8e23af1
Donaim <3Donaim@gmail.com>
2019-02-23 10:16:16 +0100

    compiles?

--- repl.py
+++ repl.py
@@ -6,10 +6,21 @@ import subprocess
 CC = 'tcc'
 PROJ = 'repl'
 SRC = PROJ + '/repl.ini'
-DEST = PROJ + '/repl.c'
+DEST = 'repl.c'
+
+def get_arguments():
+	import argparse
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+	parser.add_argument('-r', dest='refresh', action='store_true', help='Refresh contents of script file')
+	parser.add_argument('-k', dest='refresh', action='store_false', help='Keep contents of script file')
+	parser.set_defaults(refresh=False)
+
+	return parser.parse_args()
 
 TFLAGS = [
-	'--source "" --dest "{dest}"',
+	'--source', '{SRC}'.format(SRC=SRC),
+	'--dest', '{DEST}'.format(DEST=DEST),
 	'--no-make-inline',
 	'--do-caching',
 	'--no-print-intermediate',
@@ -17,27 +28,39 @@ TFLAGS = [
 	'--no-show-debug',
 	'--use-typeid',
 	'--echo-expr',
-	'--flagsfile $(PROJ)/flags.h',
-	'--headerfile $(PROJ)/header.h',
-	'--declare-file $(PROJ)/declare.h',
-	'--define-file $(PROJ)/define.c',
-	'--footerfile $(PROJ)/footer.c',
+	'--flagsfile', '{PROJ}/flags.h',
+	'--headerfile', '{PROJ}/header.h',
+	'--declare-file', '{PROJ}/declare.h',
+	'--define-file', '{PROJ}/define.c',
+	'--footerfile', '{PROJ}/footer.c',
 ]
+TFLAGS = [s.format(PROJ=PROJ) for s in TFLAGS]
 
 def kcompile():
-	subprocess.check_call(['lambda-cpp.py', '--no-make-inline'])
-	subprocess.check_call([CC, '-O0', ])
+	subprocess.check_call(['./lambda-cpp.py'] + TFLAGS)
+	subprocess.check_call([CC, '-O0', DEST, PROJ + '/header.c'])
 
-with open(SRC, 'w') as file:
+def loop(file):
 	while True:
 		inp = input('> ')
 		inp = (inp.replace('\n', ' ')).strip()
 		if inp == 'exit':
 			break
 
+		file.write(inp + '\n\n')
 		file.flush()
 		kcompile()
 
+def setup(args, callback):
+	mode = 'w' if args.refresh else 'a'
+	file = open(SRC, mode)
 
+	callback(file)
 
+def main():
+	args = get_arguments()
+	setup(args, loop)
+
+if __name__ == '__main__':
+	main()
 

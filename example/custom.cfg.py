c3a5a742009fd6f3a38cf33fa3039b71f180b357
Donaim <3Donaim@gmail.com>
2019-03-02 10:31:43 +0100

    print_true and print_false return error instead of fin

--- example/custom.cfg.py
+++ example/custom.cfg.py
@@ -3,14 +3,14 @@ class print_true:
 	def exec(x):
 		'''
 		puts("true");
-		return fin;
+		return lambda_error("Evaluated print_true");
 		'''
 
 class print_false:
 	def exec(x):
 		'''
 		puts("false");
-		return fin;
+		return lambda_error("Evaluated print_false");
 		'''
 
 class ec:

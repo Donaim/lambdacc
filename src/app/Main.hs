669ba5eb14f54910cb3d623b8ba287a26c8cf222
Donaim <3Donaim@gmail.com>
2019-03-09 23:16:11 +0100

    give up on haskell implementation
    Simple token parsing takes almost half the time whole python's parsing + emitting
    I don't want to investigate further because I dont have time
    I will try C++ next. Rust would be great fit as safe (oppose to C++) and
    fast (oppose to python/haskell) language, but I don't know it quite
    well

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -15,9 +15,14 @@ cfg = ParserConfig
 
 main :: IO ()
 main = do
-	text <- getFileText "example/script.ini"
+	text <- getFileText "example/script.inline.ini"
 	case text of
-		Just text ->
-			print $ (\toks -> toks !! (length toks - 1)) (tokenize cfg text)
+		Just text -> do
+			let toks = tokenize cfg text
+			-- print $ sum $ map (\c -> if c == 'a' then 1 else 0) text
+			-- print $ sum $ map lineno toks
+			putStrLn $ "len = " ++ show (length toks)
+			-- print $ "last: \n" ++ show (head toks)
+			return ()
 		Nothing ->
 			putStrLn "Error reading text"

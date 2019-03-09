5b3bb5d0e66f5974a798e0811a63a0481bd81464
Donaim <3Donaim@gmail.com>
2019-03-09 13:27:06 +0100

    rename ParserResult -> ParseResult

--- src/lib/Exept.hs
+++ src/lib/Exept.hs
@@ -4,14 +4,14 @@ module Exept where
 import Control.Applicative
 import Control.Monad
 
-data ParserResult a = Ok a | SyntaxError String | LogicError String
+data ParseResult a = Ok a | SyntaxError String | LogicError String
 
-instance Functor ParserResult where
+instance Functor ParseResult where
 	fmap = liftM
-instance Applicative ParserResult where
+instance Applicative ParseResult where
 	pure  = return
 	(<*>) = ap
-instance Monad ParserResult where
+instance Monad ParseResult where
 	(Ok          x) >>= f = f x
 	(SyntaxError x) >>= f = SyntaxError x
 	(LogicError  x) >>= f = LogicError x

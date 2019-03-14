94ed20c89b8af53567f655c60d8fef679f8ddf03
Donaim <3Donaim@gmail.com>
2019-03-14 21:55:06 +0100

    parse ok

--- src/lib/Parser.hs
+++ src/lib/Parser.hs
@@ -11,41 +11,41 @@ import Debug.Trace
 data Toplevel = Binding String [Token] | Expr [Token]
 	deriving (Show, Eq)
 
-parse :: ParserConfig -> [Token] -> (Toplevel, [Token])
-parse cfg [] = error "Empty"
-parse cfg (t : xs) =
-	case kind t of
-		Name -> undefined
-		_ -> undefined
+parse :: ParserConfig -> [Token] -> [Toplevel]
+parse cfg []   = error "Empty"
+parse cfg toks = groupTokens cfg toks |> map classifyGroup
 
-	where
-		afterEqual :: [Token] -> [Token]
-		afterEqual _  = []
+classifyGroup :: [Token] -> Toplevel
+classifyGroup all@(x : y : xs) =
+	if text y == "="
+	then Binding (text x) xs
+	else Expr all
+classifyGroup all = Expr all
 
-		dropSpaces = dropWhile isSpace
-		isSpace t =
-			case kind t of
-				Space -> True
-				_     -> False
-
+-- Also strips off spaces and newlines and comments
 groupTokens :: ParserConfig -> [Token] -> [[Token]]
 groupTokens _   [] = []
 groupTokens cfg toks =
-	if isEmpty
+	if null useful
 	then groupTokens cfg recur
-	else  cur : groupTokens cfg recur
+	else  useful : groupTokens cfg recur
 	where
 		curLen  = map kind toks |> takeExpr
 		cur     = take curLen toks
+		useful  = stripUseless cur
 		recur   = drop curLen toks
-		isEmpty = all isEmptyCheck cur
 
-		isEmptyCheck :: Token -> Bool
-		isEmptyCheck t = kind t == Space || kind t == Newline
-
+stripUseless :: [Token] -> [Token]
+stripUseless toks = filter (isUseful . kind) toks
+	where
+		isUseful :: TokenType -> Bool
+		isUseful Space   = False
+		isUseful Newline = False
+		isUseful Comment = False
+		isUseful _       = True
+		
 takeExpr :: [TokenType] -> Int
 takeExpr [] = 0
-
 takeExpr (Newline : Space : xs) = 2 + takeExpr xs
 takeExpr (Newline : xs)         = 1
 takeExpr (x : xs)               = 1 + takeExpr xs

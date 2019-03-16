1b9f20839e8c16d22bf3d580e44b6a3c1fce3648
Donaim <3Donaim@gmail.com>
2019-03-16 20:43:44 +0100

    ok

--- src/lib/Lexer.hs
+++ src/lib/Lexer.hs
@@ -17,61 +17,26 @@ data Leaf =
 
 type Scope = [String]
 
-lexe :: Scope -> [Token] -> [Leaf]
-lexe scope toks = lexeSplit toks |> map (lexOne scope)
+lexe :: Scope -> Tree -> Leaf
+lexe scope (Node t) =
+	lexName scope (text t)
+lexe scope (Head []) =
+	error "WTF"
 
-lexOne :: Scope -> [Token] -> Leaf
-lexOne scope (t : ts) =
-	case kind t of
-		LambdaDecl  -> Lambda (text $ head ts) (lexe (tex : scope) (tail ts))
-		OpenBracket -> error "WTF"
-		Name        -> SubExpr $ (lexName scope tex) : lexe scope ts
-		_           -> SubExpr $ lexe scope ts
-
-	where tex = text t
+lexe scope (Head all@(Node a : Node b : ts)) =
+	case kind b of
+		LambdaSymbol -> Lambda arg $ map (lexe (arg : scope)) ts
+		_            -> SubExpr $ map (lexe scope) all
+	where
+		arg = text a
+lexe scope (Head all) =
+	SubExpr $ map (lexe scope) all
 
 lexName :: Scope -> String -> Leaf
 lexName scope tex =
 	if tex `elem` scope
 	then Variable $ Argument tex
 	else Variable $ BindingTok tex Nothing
-
--- Split body into parts based on brackets
--- Result will not have brackets anymore
-lexeSplit :: [Token] -> [[Token]]
-lexeSplit []   = []
-lexeSplit toks =
-	result : lexeSplit next
-	where
-		nextBracket = getNextBracket 0 0 toks
-		result      =
-			if nextBracket >= 0
-			then take nextBracket toks
-			else toks
-		next        =
-			if nextBracket >= 0
-			then drop nextBracket toks
-			else []
-		stripped =
-			if (kind $ head result) == OpenBracket
-			then init $ tail result
-			else result
-
-getNextBracket :: Int -> Int -> [Token] -> Int
-getNextBracket count index []       =
-	if count > 0
-	then -count
-	else index
-getNextBracket count index (t : ts) =
-	case kind t of
-		OpenBracket ->
-			getNextBracket (count + 1) (index + 1) ts
-		CloseBracket ->
-			if count == 0
-			then index
-			else getNextBracket (count - 1) (index + 1) ts
-		_ ->
-			getNextBracket count (index + 1) ts
 
 data Tree =
 	Head [Tree] | Node Token

015d7947109211ad95afb218ad524d1bbd1bf5a3
Donaim <3Donaim@gmail.com>
2019-04-13 12:28:33 +0200

    cgenerator: initial

--- src/lib/CGenerator.hs
+++ src/lib/CGenerator.hs
@@ -22,3 +22,66 @@ genFlags cfg =
 		        ]
 		applyfst (f, _) = f cfg
 
+
+getPrefix :: String -> String
+getPrefix = (++) "lambdacc_"
+
+getInitName :: String -> String
+getInitName uniqueName = getPrefix "init_" ++ uniqueName
+
+getExecName :: String -> String
+getExecName uniqueName = getPrefix "exec_" ++ uniqueName
+
+getTypeid :: String -> String
+getTypeid uniqueName = getPrefix "typeid_" ++ uniqueName
+
+getInitDecl :: String -> String
+getInitDecl initName = "ff " ++ initName ++ " (ff parent)"
+
+data StructField = 
+	StructField
+	{ leaf :: Leaf
+	, index :: Int
+	}
+
+getFields :: Leaf -> [StructField]
+getFields l =
+	case l of
+		(Lambda scope argname leafs) ->
+			collect leafs 0
+		(SubExpr scope leafs) ->
+			collect leafs 0
+		(Variable scope id) ->
+			error "Getting field of argument"
+	where
+		collect :: [Leaf] -> Int -> [StructField]
+		collect [] index = []
+		collect (v@(Variable scope id) : xs) index =
+			StructField { leaf = v, index = -1 } : collect xs index
+		collcet (l : xs) index =
+			StructField { leaf = l, index = index } : collect xs (index + 1)
+
+genTypeuuid :: CompilerConfig -> Leaf -> String -> [String]
+genTypeuuid cfg lambda lambdaName =
+	if useTypeid cfg then
+		undefined
+	else []
+
+genInitFunc :: CompilerConfig -> Leaf -> String -> [String]
+genInitFunc cfg lambda lambdaName =
+	undefined
+	where
+		uniqueName = getUniqueName lambda
+		initName = getInitName uniqueName
+		execName = getExecName uniqueName
+		decl = getInitDecl initName
+
+		fields = getFields lambda
+		numLeafs = 1 + (foldl max (-1) $ map index fields)
+
+		typeuuid :: String
+		typeuuid =
+			if useTypeid cfg then
+				let typeidName = getTypeid uniqueName
+				in "me->typeuuid = " ++ typeidName ++ ";"
+			else []

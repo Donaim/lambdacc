32730ab1119cafd39544bdb1c13f9fb022b68cd6
Donaim <3Donaim@gmail.com>
2019-04-30 10:55:33 +0200

    use GHC 8.2.2

--- src/lib/Utils.hs
+++ src/lib/Utils.hs
@@ -23,46 +23,9 @@ mapPartCxt f ctx all@(_:xs) =
 	where (newCtx, x) = f ctx all
 mapPartCxt _ _ _ = []
 
-joinMaybe :: (Applicative m, Semigroup (m a)) => (b -> Maybe (b, a)) -> (b, m a) -> (b, m a)
-joinMaybe fun prev@(s, toks) =
-	case res of
-		Just (ns, t)  -> (ns, (toks <> pure t))
-		Nothing       -> prev
-	where res = fun s
-
-joinMaybeEm :: (Monoid b, Eq b, Applicative m, Semigroup (m a)) => (b -> Maybe (b, a)) -> (b, m a) -> (b, m a)
-joinMaybeEm fun prev@(s, toks)
-	| s == mempty = prev
-	| otherwise   = joinMaybe fun prev
-
-joinMaybeAll :: (Monoid b, Eq b, Applicative m, Monoid (m a), Foldable f) => b -> f (b -> Maybe (b, a)) -> m a
-joinMaybeAll initial funcs =
-	snd $ tokenizebuf (initial, mempty)
-	where
-		tokenizebuf (s, b)
-			| s == mempty = (s, b)
-			| otherwise   = tokenizebuf $ foldr joinMaybeEm (s, b) funcs
-
-joinMaybeSeq :: (Monoid b, Eq b) => b -> [(b -> Maybe (b, a))] -> [a]
-joinMaybeSeq initial funcs =
-	toktok initial
-	where
-		toktok s =
-			if s == mempty then
-				[]
-			else
-				case mhead $ catMaybes m of
-					Nothing ->
-						[]
-					Just (news, newt) ->
-						newt : toktok (news)
-			where
-				m = map (\f -> f s) funcs
-
 padWith :: Char -> Int -> String -> String
 padWith padS padN str =
 	str ++ (replicate (max 0 $ padN - length str) padS)
-
 
 trim xs = dropSpaceTail "" $ dropWhile C.isSpace xs
 	where

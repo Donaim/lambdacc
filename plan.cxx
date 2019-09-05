491e0cc0dcfccff99946374061f32642aa5036a4
Donaim <3Donaim@gmail.com>
2019-09-05 20:25:54 +0200

    stacking plan

--- plan.cxx
+++ plan.cxx
@@ -1,15 +1,15 @@
 
+#define NULL 0
 typedef unsigned char bool;
+
 struct term;
 
 typedef struct term* termp_t;
-typedef termp_t (*child_constructor_t)(termp_t, termp_t); /* accepts parent and parent argument to copy to scope */
+typedef termp_t (*child_constructor_t)(termp_t*, termp_t); /* accepts parent scope and parent argument to copy to childs scope */
 
 enum BODYPART_TYPE {
 	BODYPART_TYPE_IN_SCOPE,
-	BODYPART_TYPE_IS_ABSTRACTION_ARGUMENT,
 	BODYPART_TYPE_CONSTRUCTIBLE,
-	BODYPART_TYPE_EMPTY,
 };
 
 union child {
@@ -17,56 +17,78 @@ union child {
 	child_constructor_t constructor;
 };
 
-typedef void stack;
+typedef void* stack_t;
 
+void stack_push(stack_t stack, void *ptr);
+void* stack_pop(stack_t stack);
+stack_t stack_new(void);
+
+/* TODO: optimize in terms of size */
 struct term {
-	union child left;
-	enum BODYPART_TYPE left_type;                                /* TODO: use single type right (reuse index_in_scope) */
+	union {
+		struct {
+			union child left;
+			enum BODYPART_TYPE left_type;
 
-	union child right;
-	enum BODYPART_TYPE right_type;                               /* TODO: use single type right (reuse index_in_scope) */
+			union child right;
+			enum BODYPART_TYPE right_type;
+		} application;
+		struct {
+			union child b;
+			enum BODYPART_TYPE btype;
+		} abstraction;
+	} body;
+	bool is_abstraction;
 
-	termp_t *scope;                                              /* TODO: allocate together with struct */
+	termp_t *scope;                                                      /* TODO: allocate together with struct */
 };
+
+/* (x . ((y . y) x) x) 3
+ */
 
 termp_t reduce(termp_t me0, termp_t x0) {
 	termp_t me = me0;
-	termp_t x = x0;
+	termp_t right = x0;
+	stack_t stack = stack_new();
 
-	while (1) {
-		termp_t right;
-		termp_t left;
-
-		switch (me->left_type) {
-			case BODYPART_TYPE_IN_SCOPE:
-				left = me->scope[me->left.index_in_scope];
-				break;
-			case BODYPART_TYPE_IS_ABSTRACTION_ARGUMENT:
-				left = x;
-				break;
-			case BODYPART_TYPE_CONSTRUCTIBLE:
-				left = me->left.constructor(me, x);
-				break;
-		}
-		switch (me->right_type) {
-			case BODYPART_TYPE_IN_SCOPE:
-				right = me->scope[me->right.index_in_scope];
-				break;
-			case BODYPART_TYPE_IS_ABSTRACTION_ARGUMENT:
-				right = x;
-				break;
-			case BODYPART_TYPE_CONSTRUCTIBLE:
-				right = me->right.constructor(me, x);
-				break;
-		}
-
-		if (me->right_type == BODYPART_TYPE_EMPTY) {
-			me = left;
-			break;
+	loop: {
+		if (me->is_abstraction) {
+			switch (me->body.abstraction.btype) {
+				case BODYPART_TYPE_IN_SCOPE:
+					me = me->scope[me->body.abstraction.b.index_in_scope];
+					goto maybeend;
+				case BODYPART_TYPE_CONSTRUCTIBLE:
+					me = me->body.abstraction.b.constructor(me->scope, right);
+					break;
+			}
 		} else {
-			me = left;
-			x = right;
+			stack_push(stack, right);
+
+			switch (me->body.application.right_type) {
+				case BODYPART_TYPE_IN_SCOPE:
+					right = me->scope[me->body.application.right.index_in_scope];
+					break;
+				case BODYPART_TYPE_CONSTRUCTIBLE:
+					right = me->body.application.right.constructor(me->scope, NULL);
+					break;
+			}
+			switch (me->body.application.left_type) {
+				case BODYPART_TYPE_IN_SCOPE:
+					me = me->scope[me->body.application.left.index_in_scope];
+					break;
+				case BODYPART_TYPE_CONSTRUCTIBLE:
+					me = me->body.application.left.constructor(me->scope, NULL);
+					break;
+			}
 		}
+
+		goto loop;
+	}
+
+maybeend:
+	right = stack_pop(stack);
+	if (right) {
+		goto loop;
 	}
 
 	return me;
